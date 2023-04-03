4a16ba8749596d5ebcee1f36226cd427173b01b9
Nguyeen cop code laf trum <129312516+Nguyensieucopcode@users.noreply.github.com>
2023-04-03 18:49:05 +0700

    Create voi.cpp

---
+++ voi.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define fill(lt,x) memset(lt,x,sizeof(lt))
+const int N = 1e5+7 , mod = 1000000007;
+int arr[N+7],f[N+7],rt=0;
+vector< vector<int> > a(N),v(N);
+
+void dfs(int id)
+{
+    if (f[id]) return;
+    f[id]=1;
+    v[rt].push_back(id);
+    for (int i:a[id]) dfs(i);
+}
+
+signed main()
+{
+    cin.tie(0) -> sync_with_stdio(false);
+    #define task "voi"
+    if (fopen(task".inp","r"))
+    {
+        (void)freopen(task".inp","r",stdin);
+        (void)freopen(task".out","w+",stdout);
+    }
+    int n,m; cin>>n>>m;
+    for (int i=1; i<=n; i++) cin>>arr[i];
+    while (m--)
+    {
+        int u,v; cin>>u>>v;
+        a[u].push_back(v); a[v].push_back(u);
+    }
+    fill(f,0);
+    for (int i=1; i<=n; i++)
+    {
+        if (!f[i])
+        {
+            rt++; dfs(i);
+        }
+    }
+    int ans = 1, res = 0;
+    for (int i=1; i<=rt; i++)
+    {
+        int mi = (int)1e10 , t = 0;
+        for (int j : v[i]) mi=min(mi,arr[j]);
+        res = (res % mod + mi % mod) % mod;
+        for (int j : v[i]) t = ( t % mod + (arr[j] == mi) % mod) % mod;
+        ans = ( (ans % mod) * (t % mod) ) % mod;
+    }
+    cout<<res<<"\n"<<ans;
+}
